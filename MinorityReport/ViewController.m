b36f74862b053b366b47246ce0c13fc4454b4c12
Edil Ashimov <thekgzteam@gmail.com>
2015-09-16 18:53:46 -0500

    all changes

--- MinorityReport/ViewController.m
+++ MinorityReport/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *theFutureLabel;
+@property (weak, nonatomic) IBOutlet UILabel *PreCogLabel;
+@property CGPoint originalCenter;
 @end
 
 @implementation ViewController
@@ -17,11 +19,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    self.originalCenter = self.theFutureLabel.center;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+- (IBAction)panHandler:(UIGestureRecognizer *)handGesture
 
+{
+    if (handGesture.state == UIGestureRecognizerStateEnded){
+        [UIView animateWithDuration: 0.5 animations:^
+         {
+             self.theFutureLabel.center = self.originalCenter;
+             self.theFutureLabel.text = @"Go Again!";
+             self.theFutureLabel.backgroundColor = [UIColor grayColor];
+             [self.theFutureLabel sizeToFit];
+             self.PreCogLabel.backgroundColor = [UIColor redColor];
+         }];
+    }
+    else
+    {
+        CGPoint point = [handGesture locationInView:self.view];
+        self.theFutureLabel.center = point;
+
+        if (CGRectContainsPoint(self.PreCogLabel.frame, point)) {
+
+            self.theFutureLabel.text = @"afictitiousandincriminatingfuture";
+            self.theFutureLabel.backgroundColor = [UIColor redColor];
+            [self.theFutureLabel sizeToFit];
+            NSLog(@"panning");
+        }
+    }
+    
+}
 @end
+
+
+
+
+
+
+
+
+
+
+
